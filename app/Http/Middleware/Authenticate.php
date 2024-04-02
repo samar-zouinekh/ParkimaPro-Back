506c10dd26cdd95e372c5bef9af4b45ec70404ef
samar.zouinekh <samarzouinkh@gmail.com>
2024-04-02 10:20:57 +0100

    fix redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,9 +15,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-            // return redirect(RouteServiceProvider::HOME);
-        }
+        // if (! $request->expectsJson()) {
+        //     return route('login');
+        // }
     }
 }
