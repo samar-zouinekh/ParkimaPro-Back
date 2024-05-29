f5b6ed99375fa1d4a326ab9bd68a5b0388de6e7d
samar.zouinekh <samarzouinkh@gmail.com>
2024-05-29 15:12:02 +0100

    plate list

--- app/Http/Controllers/LicensePlateController.php
+++ app/Http/Controllers/LicensePlateController.php
@@ -331,7 +331,7 @@ class LicensePlateController extends Controller
 
             $tab2 = [];
             foreach ($tab as $expiry_date => $item) {
-                dump($tab, $expiry_date, $item);
+                dump($$tab2[$expiry_date], $expiry_date, $item);
                 if (!empty($tab2[$expiry_date])) {
                     array_push($tab2, $item);
                 }
